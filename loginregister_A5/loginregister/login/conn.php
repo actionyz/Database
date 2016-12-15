ef3f0871b32c7fcce22cd94235cebb87dc1b370d
actionyz <1021533940@qq.com>
2016-12-15 19:36:45 +0800

    first_commit

--- loginregister_A5/loginregister/login/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-//数据库链接文件
-$host='localhost';//数据库服务器
-$user='root';//数据库用户名
-$password='111111';//数据库密码
-$database='college2';//数据库名
-$connect = mysqli_connect($host,$user,$password) or die ('Connect Error'.mysqli_connect_error());
-mysqli_select_db($connect,$database) or die ('Select Error');
-?>
